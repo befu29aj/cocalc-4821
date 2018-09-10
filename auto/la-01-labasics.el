7987eb1328103cbaaa52aa5dc8b84159c722b5f8
Stefan Lukits <sediomyle@gmail.com>
2018-09-10 09:20:54 -0700

    la-01-labasics.tex

--- auto/la-01-labasics.el
+++ auto/la-01-labasics.el
@@ -3,6 +3,15 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -31,10 +40,35 @@
     "eq:queiyesh"
     "eq:quesieha"
     "eq:iegoowae"
+    "eq:aengixoh"
+    "eq:eecheipo"
     "eq:ohghohfi"
     "eq:cegeemoi"
     "eq:vaishien"
     "eq:phoaxoze"
+    "eq:mohloogh"
+    "eq:xaigeeke"
+    "eq:ahshohwa"
+    "eq:yeghahpi"
+    "eq:oamaiwei"
+    "eq:kaiquaeb"
+    "eq:fiesebah"
+    "eq:weaseiwa"
+    "eq:yaetooni"
+    "eq:jichoora"
+    "eq:iewoobih"
+    "eq:phaiyahj"
+    "eq:waduifee"
+    "eq:weiyushe"
+    "eq:jiadaush"
+    "eq:ohneirae"
+    "eq:wahheedo"
+    "eq:zeegheir"
+    "eq:ooleeyae"
+    "eq:ieghoisa"
+    "eq:eighilai"
+    "eq:thidaitu"
+    "eq:ohkaicux"
     "eq:ahgohcoh"
     "eq:neithohn"
     "eq:baixieda"
