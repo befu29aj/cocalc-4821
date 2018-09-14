79ff795be797592b6ebc353a107df2df2b8ac00a
Stefan Lukits <sediomyle@gmail.com>
2018-09-14 15:30:27 -0700

    la-03-vectors.tex

---
+++ auto/la-03-vectors.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "la-03-vectors"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "alltt")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "teachbeamer")
+   (LaTeX-add-labels
+    "eq:lapheeka"
+    "eq:kaepuema"
+    "eq:kemodaim"
+    "eq:bahniech"
+    "eq:teequahg"
+    "eq:zaegiexo"
+    "eq:ogeithie"
+    "eq:ahnoocae"
+    "eq:oobeipho"
+    "eq:aiwahzoa"
+    "eq:tauhohju"
+    "eq:ijaquahd"
+    "eq:ohyizaeb"
+    "eq:yohsheen"
+    "eq:ijaeriri"
+    "eq:bamoyeez"
+    "eq:abeekohc"
+    "eq:eiyeigaz"
+    "eq:yohghaef"
+    "eq:oxeingiu"
+    "eq:ukohjiej"
+    "eq:maeshael"
+    "eq:ushahroh"
+    "eq:iefeeboh"
+    "eq:vetiexup"
+    "eq:eechawoi"
+    "eq:quaghoob"
+    "eq:yeibieba"))
+ :latex)
+
