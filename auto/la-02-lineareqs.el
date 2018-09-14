8936d35c8a1dffeaa61a50909bd4ad2df04b3312
Stefan Lukits <sediomyle@gmail.com>
2018-09-14 15:27:01 -0700

    la-02-lineareqs.tex

---
+++ auto/la-02-lineareqs.el
@@ -0,0 +1,53 @@
+(TeX-add-style-hook
+ "la-02-lineareqs"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor=dvipsnames")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "teachbeamer")
+   (LaTeX-add-labels
+    "eq:mohloogh"
+    "eq:xaigeeke"
+    "eq:ahshohwa"
+    "eq:yeghahpi"
+    "eq:oamaiwei"
+    "eq:kaiquaeb"
+    "eq:fiesebah"
+    "eq:weaseiwa"
+    "eq:yaetooni"
+    "eq:jichoora"
+    "eq:iewoobih"
+    "eq:phaiyahj"
+    "eq:waduifee"
+    "eq:weiyushe"
+    "eq:jiadaush"
+    "eq:ohneirae"
+    "eq:wahheedo"
+    "eq:zeegheir"
+    "eq:ooleeyae"
+    "eq:ieghoisa"
+    "eq:eighilai"
+    "eq:thidaitu"
+    "eq:ohkaicux"
+    "eq:ahgohcoh"
+    "eq:neithohn"
+    "eq:baixieda"
+    "eq:maethung"
+    "eq:leighuga"
+    "eq:aurohbac"
+    "eq:nahshooh"
+    "eq:aecocaeh"
+    "eq:weeraesh"
+    "eq:ieyoongu"
+    "eq:ephoopha"
+    "eq:oogeujie"
+    "eq:seeleeje"
+    "eq:oemeyeib"
+    "eq:eicheuth"
+    "eq:iephaeke"
+    "eq:euseeyee"))
+ :latex)
+
