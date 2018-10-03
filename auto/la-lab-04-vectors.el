442238c96efb06ac6a8e8245663d8326d66fc333
Stefan Lukits <sediomyle@gmail.com>
2018-10-02 17:37:49 -0700

    la-lab-04-vectors.tex

---
+++ auto/la-lab-04-vectors.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "la-lab-04-vectors"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "enumerate"
+    "syllogism"
+    "october"
+    "xcolor")
+   (TeX-add-symbols
+    '("aufgabe" 0))
+   (LaTeX-add-labels
+    "eq:ilishaih"
+    "eq:ookoohos"
+    "eq:oobaisoo"
+    "eq:quiequae"
+    "eq:uchaelex"
+    "eq:hohnooph"
+    "eq:iozooroo")
+   (LaTeX-add-counters
+    "aufg"))
+ :latex)
+
