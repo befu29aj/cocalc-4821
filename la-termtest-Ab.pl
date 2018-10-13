615145140e8b6463ee898631050ece401a771a3a
Stefan Lukits <sediomyle@gmail.com>
2018-10-12 17:01:20 -0700

    la-termtest-Ab.pl

--- la-termtest-Ab.pl
+++ la-termtest-Ab.pl
@@ -71,11 +71,11 @@ $aufgabe[1]=sub {
 };
 
 $aufgabe[2]=sub {
-    print termtest "HELP! Determine how many solutions the following system of equations has. Your answer should be none, one, or infinitely many. Show your work.$v005[$i1]\n\n";
+    print termtest "Determine how many solutions the following system of equations has. Your answer should be none, one, or infinitely many. Show your work.$v005[$i1]\n\n";
 };
 
 $aufgabe[3]=sub {
-    print termtest "Convert the following complex number to polar form: \$\\frac{$v006[$i1]}{$v007[$i1]}\$.";
+    print termtest "Convert the following complex number to polar form: \\begin{equation}\\frac{$v006[$i1]}{$v007[$i1]}\\notag\\end{equation}";
 };
 
 for ($y2=0;$y2<=$#aufgabe;++$y2)
@@ -107,8 +107,8 @@ $v004[1]="\\begin{equation}\n\\begin{array}{ccccccc}\n2a&+&3b&+&7c&=&-1\\\\\n4a&
 $v005[0]="\\begin{equation}\n\\begin{array}{ccccccc}\n-4x&+&2y&+&6z&=&4\\\\\n8x&-&8y&-&z&=&2\\\\\n-4x&-&2y&+&17z&=&9\n\\end{array}\\notag\n\\end{equation}";
 $v005[1]="\\begin{equation}\n\\begin{array}{ccccccc}\n3x&+&4y&-&8z&=&-6\\\\\n-x&+&3y&+&6z&=&7\\\\\nx&+&10y&+&4z&=&-3\n\\end{array}\\notag\n\\end{equation}";
 $v006[0]="-2+2i";
-$v006[1]="5+i";
-$v007[0]="3-3i";
+$v006[1]="3-3i";
+$v007[0]="5+i";
 $v007[1]="-4+i";
 $v008[0]="";
 $v008[1]="";
